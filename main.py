c41037179b9be4aabe004578629703ff8a542cee
Daumián Ruiz <daumianruiz@gmail.com>
2023-04-16 05:18:58 -0300

    V03
    deje solo lo imporante, limpie la data y la deje aparte, ya funciona uno de los fast api

--- main.py
+++ main.py
@@ -1,54 +1,77 @@
+import pandas as pd
+
 from typing import Optional
 from pydantic import BaseModel
+from fastapi import FastAPI,Body,Query
 
-from fastapi import FastAPI,Body
 
 #creacion de la app
 app:FastAPI = FastAPI();
 
-#Modelo
-class Person(BaseModel):              
-   first_name: str 
-   last_name: str
-   age: int
-   hair_color: Optional[str] = None
-   is_married: Optional[bool] = None
+amazon_df = pd.read_csv('df_limpios\DF_Amazon.csv')
+disney_df = pd.read_csv('df_limpios\DF_Disney.csv')
+hulu_df = pd.read_csv('df_limpios\DF_Hulu.csv')
+netflix_df = pd.read_csv('df_limpios\DF_Netflix.csv')
+
+def contar_datos(dataframe):
+   if dataframe == "1" or dataframe == "amazon":
+      return amazon_df.shape[0]
+   elif dataframe == "2" or dataframe == "netflix":
+      return netflix_df.shape[0]
+   elif dataframe == "3" or dataframe == "disney":
+      return disney_df.shape[0]
+   elif dataframe == "4" or dataframe == "hulu":
+      return hulu_df.shape[0]
+   else:
+      return "error:plataforma incorrecta"
 
 
 #appdef
 @app.get("/")
 async def home():
-    return {"Hola": "Mundo"}
+   return {"Hola": "Mundo"}
 
-#crearPersona
-@app.post("person/new")
-def create_person(person: Person = Body(...)):
-   return person
+#Cuantas Peliculas hay en esta plataforma
+@app.get("/get_count_platform/{plataforma}")
+async def plataforma(plataforma:str):
 
+   cantidad=contar_datos(plataforma)
 
-@app.get("/get_max_duration")
-async def boton1():
-    return {"message": "Has presionado el botón 1"}
-
-@app.get("/get_score_count")
-async def boton2():
-    return {"message": "Has presionado el botón 2"}
-
-@app.get("/get_count_platform")
-async def boton2():
-    return {"message": "Has presionado el botón 2"}
-
-@app.get("/prod_per_county")
-async def boton2():
-    return {"message": "Has presionado el botón 2"}
-
-@app.get("/get_contents")
-async def boton2():
-    return {"message": "Has presionado el botón 2"}
+   return {"la plataforma":{plataforma},
+            "tiene un total de peliculas": cantidad}
 
 
-    #get_max_duration(year, platform, duration_type)
+# @app.get("/get_max_duration")
+# def boton1():
+#     return {"message": "Has presionado el botón 1"}
+
+# @app.get("/get_score_count")
+# def boton2():
+#     return {"message": "Has presionado el botón 2"}
+
+
+#Cuantas Peliculas hay en esta plataforma
+
+# @app.get("/get_count_platform/{plataforma}")
+
+# async def plataforma(plataforma:str):
+#    cantidad_peliculas=sin_jupiter.variable3
+#    return {"plataforma":cantidad_peliculas}
+
+# @app.get("/prod_per_county")
+# def boton2():
+#     return {"message": "Has presionado el botón 2"}
+
+# @app.get("/get_contents")
+# def boton2():
+#     return {"contenido"}
+
+
+#get_max_duration(year, platform, duration_type)
 #get_score_count(platform, scored, year)
 #get_count_platform(platform)
 #prod_per_county(tipo,pais,anio)
 #get_contents(rating)
+
+
+
